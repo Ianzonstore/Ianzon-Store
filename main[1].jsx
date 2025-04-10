86b4f57464fac5e1a4d6e93ee42082df1a2c70c4
Ianzonstore <ianzonstore@gmail.com>
2025-04-10 17:48:31 -0400

    Add files via upload

---
+++ main[1].jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IanzonStoreLanding from './IanzonStoreLanding.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IanzonStoreLanding />
+  </React.StrictMode>,
+)
